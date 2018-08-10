e418adfeab0f96519696eaec44eec2f358c12e1d
christianampe <christianampe.work@gmail.com>
2018-08-10 15:40:55 -0700

    began constructing credit card default styling

--- FlatPaymentMethod/Classes/CreditCardViewConfig.swift
+++ FlatPaymentMethod/Classes/CreditCardViewConfig.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import PaymentValidator
 
 public struct CreditCardViewConfig {
     public let backgroundColor: UIColor
@@ -22,6 +23,94 @@ public struct CreditCardViewConfig {
 public enum CreditCardStyle {
     case fill(backgroundColor: UIColor)
     case clear(borderWidth: CGFloat, borderColor: UIColor)
+}
+
+public extension CreditCardStyle {
+    var backgroundColor: UIColor {
+        switch self {
+        case .clear:
+            return .clear
+        case .fill(let backgroundColor):
+            return backgroundColor
+        }
+    }
+    
+    var borderWidth: CGFloat {
+        switch self {
+        case .clear(let borderWidth, _):
+            return borderWidth
+        case .fill:
+            return 0
+        }
+    }
+    
+    var borderColor: UIColor {
+        switch self {
+        case .clear(_, let borderColor):
+            return borderColor
+        case .fill:
+            return .clear
+        }
+    }
+    
+    var textColor: UIColor {
+        switch self {
+        case .clear:
+            return .black
+        case .fill:
+            return .white
+        }
+    }
+}
+
+public extension CreditCardStyle {
+    func logo(for cardType: CreditCardType) -> UIImage {
+        switch self {
+        case .clear:
+            guard let colorLogo = UIImage(named: cardType.logoName) else {
+                assert(false, "color logo for \(cardType.name) not found")
+                return UIImage()
+            }
+            
+            return colorLogo
+        case .fill:
+            guard let lightLogo = UIImage(named: "\(cardType.logoName)-light") else {
+                assert(false, "light logo for \(cardType.name) not found")
+                return UIImage()
+            }
+            
+            return lightLogo
+        }
+    }
+}
+
+private extension CreditCardType {
+    var logoName: String {
+        switch self {
+        case .amex:
+            return "amex"
+        case .dankort:
+            return "dankort"
+        case .dinersClub:
+            return "diners-club"
+        case .discover:
+            return "discover"
+        case .interPayment:
+            return "inter-payment"
+        case .maestro:
+            return "maestro"
+        case .masterCard:
+            return "mastercard"
+        case .jcb:
+            return "jcb"
+        case .uatp:
+            return "uatp"
+        case .unionPay:
+            return "union-pay"
+        case .visa:
+            return "visa"
+        }
+    }
 }
 
 public enum CardNumberSecurityStyle {
