5b04aa1ed83b1cf8fa24b5beff1003979298b7f8
christianampe <christianampe.work@gmail.com>
2018-08-09 17:25:16 -0700

    moved payment validator into its own dependency

--- Example/Pods/Target Support Files/Pods-FlatPaymentMethod_Example/Pods-FlatPaymentMethod_Example-frameworks.sh
+++ Example/Pods/Target Support Files/Pods-FlatPaymentMethod_Example/Pods-FlatPaymentMethod_Example-frameworks.sh
@@ -144,9 +144,11 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/FlatPaymentMethod/FlatPaymentMethod.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/PaymentValidator/PaymentValidator.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/FlatPaymentMethod/FlatPaymentMethod.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/PaymentValidator/PaymentValidator.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
